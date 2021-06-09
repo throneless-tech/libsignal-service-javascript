1d40d7245fa627187c8c489f92c2f96d7ef9b5a8
Josh King <jking@chambana.net>
2021-06-09 16:45:05 -0400

    Fix crypto helper functions.

--- src/utils.ts
+++ src/utils.ts
@@ -1,16 +1,16 @@
 import { textsecure } from '../lib/ts/textsecure/index';
 import utils from '../lib/ts/textsecure/Helpers';
-import libsignal from './js/libsignal';
+import { libsignal } from './js/libsignal';
 
 export function generatePassword() {
   const password = btoa(
-    utils.getString(libsignal.KeyHelper.getRandomBytes(16))
+    utils.getString(libsignal.crypto.getRandomBytes(16))
   );
   return password.substring(0, password.length - 2);
 }
 
 export function generateGroupId() {
-  return utils.getString(libsignal.KeyHelper.getRandomBytes(16));
+  return utils.getString(libsignal.crypto.getRandomBytes(16));
 }
 
 export function getCredentials(): [string, string] {
